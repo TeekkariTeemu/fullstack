9daa191865506101e6290305499938ccb8a75850
TeekkariTeemu <tsoukki@hotmail.com>
2023-03-06 15:37:24 +0200

    Suoritettiin enimmäinen tehtävä

--- Osa1/kurssitiedot/src/App.js
+++ Osa1/kurssitiedot/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const Header = (props) => {
+  console.log(props)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{props.name}</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+const Content = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>{props.name} {props.value}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Number of exercises {props.one + props.two + props.three}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header name={course}/>
+      <Content name={part1} value= {exercises1}/>
+      <Content name={part2} value= {exercises2}/>
+      <Content name={part3} value= {exercises3}/>
+      <Total one={exercises1} two={exercises2} three={exercises3}/>
+    </div>
+  )
+}
+
+export default App
