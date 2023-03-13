f75199ecd2167326fb89d307ae1f6b8b53303598
TeekkariTeemu <tsoukki@hotmail.com>
2023-03-13 15:55:17 +0200

    Osa 1c muutokset

--- Osa1/part1/src/App.js
+++ Osa1/part1/src/App.js
@@ -1,21 +1,49 @@
-const Hello = (props) => {
-  console.log(props)
+import { useState } from 'react'
+
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => { 
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
+
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
+      <Display counter={counter}/>
+      <Button
+        handleClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        handleClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        handleClick={decreaseByOne}
+        text='minus'
+      />  
     </div>
   )
 }
 
-const App = () => {
-  const nimi = 'Pekka'
-  const ika = 10
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={nimi} age={ika} />
-    </div>
-  )
-}
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
 export default App
