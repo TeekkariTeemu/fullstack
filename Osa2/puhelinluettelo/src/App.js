7733c752499f9bafc4937668abf21ddf39dd5aa0
TeekkariTeemu <tsoukki@hotmail.com>
2023-04-13 11:04:40 +0300

    2.11 valmis

--- Osa2/puhelinluettelo/src/App.js
+++ Osa2/puhelinluettelo/src/App.js
@@ -1,18 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect  } from 'react'
 import List from './Components/List'
 import Filter from './Components/Filter'
+import axios from 'axios'
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
   const [toShow, setToShow] = useState(persons)
 
+  useEffect(() => {
+    console.log('effect')
+    const eventHandler = response => {
+      console.log('promise fulfilled')
+      setPersons(response.data)
+      setToShow(response.data)
+    }
+    const promise = axios.get('http://localhost:3001/persons')
+    promise.then(eventHandler)
+  }, [])
+
+  console.log('render', persons.length, 'persons')
   console.log("filter1", filter)
   const addName = (event) => {
     event.preventDefault()
