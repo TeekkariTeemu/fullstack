0b00874c327c86363c36dcc121363e26d93a9462
TeekkariTeemu <tsoukki@hotmail.com>
2023-05-03 13:28:09 +0300

    3.3 valmis

--- Osa3/puhelinluettelo_backend/index.js
+++ Osa3/puhelinluettelo_backend/index.js
@@ -52,7 +52,11 @@ let persons = [
   app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     const person = persons.find(person => person.id === id)
-    response.json(person)
+    if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
   })
 
   app.get('/info', (request, response) => {
