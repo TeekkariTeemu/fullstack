a9493044fe4e5a12f1dfa2eeeb84bcbe935c1514
TeekkariTeemu <tsoukki@hotmail.com>
2023-05-22 15:15:33 +0300

    Osa4 aloitettu. Valmistelut ennen 4.1 tehty

---
+++ Osa4/puhelinluettelo_backend/index.js
@@ -0,0 +1,124 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
+const Person = require('./models/person')
+
+
+
+//tehdään tokeni, joka sisältää POST:in datan,
+//jos pyyntö on tyyppiä POST
+morgan.token('postData', (request) => {
+  if (request.method === 'POST') {
+    return JSON.stringify(request.body)
+  }
+  return ''
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(express.static('build'))
+app.use(express.json())
+app.use(cors())
+//mukaillaan tiny-konfiguraatiota, morgan-dokumentaation mukaisesti
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'))
+
+
+//Pyyntöön vastataan response-olion metodilla json, joka
+//lähettää HTTP-pyynnön vastaukseksi parametrina olevaa
+//JavaScript-olioa eli taulukkoa notes vastaavan
+//JSON-muotoisen merkkijonon
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
+})
+
+//Nyt app.get('/api/notes/:id', ...)käsittelee kaikki HTTP GET
+//-pyynnöt, jotka ovat muotoa /api/notes/JOTAIN,
+//jossa JOTAIN on mielivaltainen merkkijono.
+//Polun parametrin id arvoon päästään käsiksi pyynnön
+//tiedot kertovan olion request kautta
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+//poistaa nimen listasta
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+
+//Postataan uusi nimi ja numero. (jos molemmat täytetty)
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+    .catch(error => next(error))
+})
+
+app.get('/info', async (request, response) => {
+  const date = new Date()
+  const len = await Person.countDocuments()
+  response.send(`Phonebook has info for ${len} people<br>${date}`)
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
+//rivit sitovat muuttujaan app sijoitetun http-palvelimen
+//kuuntelemaan porttiin 3002 tulevia HTTP-pyyntöjä
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
