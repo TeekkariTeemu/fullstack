7b29c8550dd5db0bc7f22fef43f7451b5883d261
TeekkariTeemu <tsoukki@hotmail.com>
2023-05-26 15:31:05 +0300

    4.8 valmis

---
+++ Osa4/blogilista/backend/tests/test_helper.js
@@ -0,0 +1,49 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: 'First Blog',
+    author: 'John Doe',
+    url: 'https://example.com/first-blog',
+    likes: 10,
+    id: '646cbb9212eb6e454eb6397b'
+  },
+  {
+    title: 'Second Blog',
+    author: 'Jane Smith',
+    url: 'https://example.com/second-blog',
+    likes: 5,
+    id: '789abcde1234567890'
+  },
+  {
+    title: 'Third Blog',
+    author: 'John Doe',
+    url: 'https://example.com/third-blog',
+    likes: 8,
+    id: '0987654321fedcba'
+  },
+  {
+    title: 'Fourth Blog',
+    author: 'Alice Johnson',
+    url: 'https://example.com/fourth-blog',
+    likes: 3,
+    id: 'a1b2c3d4e5f6g7h8i9j0'
+  }
+]
+
+const nonExistingId = async () => {
+  const blog = new Blog({ content: 'willremovethissoon' })
+  await blog.save()
+  await blog.remove()
+
+  return blog._id.toString()
+}
+
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(blog => blog.toJSON())
+}
+
+module.exports = {
+  initialBlogs, nonExistingId, blogsInDb
+}
