edc668fa413dce98fa391f601ea42e0aa6ad12b3
TeekkariTeemu <tsoukki@hotmail.com>
2023-05-26 13:14:23 +0300

    ennen 4.8 alkua

--- Osa4/notebackend/mongo.js
+++ Osa4/notebackend/mongo.js
@@ -10,25 +10,29 @@ const password = process.argv[2]
 const url =
   `mongodb+srv://TeekkariTeemu:${password}@cluster0.rrlyipp.mongodb.net/noteApp?retryWrites=true&w=majority`
 
-mongoose.set('strictQuery', false)
+mongoose.set('strictQuery',false)
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
   content: String,
+  date: Date,
   important: Boolean,
 })
 
 const Note = mongoose.model('Note', noteSchema)
 
-// const note = new Note({
-//    content: 'HTML is Easy',
-//    important: true,
-//  })
+const note = new Note({
+  content: 'Mongoose makes things easy',
+  date: new Date(),
+  important: true,
+})
 
-//  note.save().then(result => {
-//    console.log('note saved!')
-//    mongoose.connection.close()
-//  })
+/*
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
+*/
 
 Note.find({}).then(result => {
   result.forEach(note => {
