d538e0238c564327fd9191cb447f34bdd5f5b4b0
TeekkariTeemu <tsoukki@hotmail.com>
2023-06-06 14:48:32 +0300

    4.19 valmis

--- Osa4/blogilista/backend/controllers/blogs.js
+++ Osa4/blogilista/backend/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -9,14 +10,27 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
   try {
-    const user = await User.findById(body.userId)
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
 
     const blog = new Blog({
       title: body.title,
+      author: body.author,
       url: body.url,
       likes: body.likes,
       user: user._id
@@ -45,6 +59,7 @@ blogsRouter.put('/:id', (request, response) => {
 
   const  blog = {
     title: body.title,
+    author: body.author,
     url: body.url,
     likes: body.likes,
     user: body.user
