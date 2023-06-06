4206488018d7f5e1fad096fa90960898a3761531
TeekkariTeemu <tsoukki@hotmail.com>
2023-06-06 12:48:43 +0300

    4.15 tehty

--- Osa4/blogilista/backend/controllers/blogs.js
+++ Osa4/blogilista/backend/controllers/blogs.js
@@ -1,27 +1,31 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
-blogsRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
+
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
+blogsRouter.post('/', async (request, response) => {
+  const body = request.body
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-    .catch(error => {
-      if (error.name === 'ValidationError') {
-        response.status(400).json({ error: error.message })
-      }
-    })
+  const user = await User.findById(body.userId)
+
+  const blog = new Blog({
+    title: body.title,
+    url: body.url,
+    likes: body.likes,
+    user: user._id
+  })
+
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  response.json(savedBlog)
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
@@ -35,7 +39,8 @@ blogsRouter.put('/:id', (request, response) => {
   const  blog = {
     title: body.title,
     url: body.url,
-    likes: body.likes
+    likes: body.likes,
+    user: body.user
   }
 
   Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
