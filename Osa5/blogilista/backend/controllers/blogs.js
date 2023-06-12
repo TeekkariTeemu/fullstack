7c2a2fd6ebf091426e9586cacb7c065f84ad7d0d
TeekkariTeemu <tsoukki@hotmail.com>
2023-06-12 14:27:19 +0300

    5.1 alustettu

---
+++ Osa5/blogilista/backend/controllers/blogs.js
@@ -0,0 +1,85 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const middleware = require('../utils/middleware')
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
+
+  response.json(blogs)
+})
+
+blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
+  const body = request.body
+
+  try {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = request.user
+
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+      user: user._id
+    })
+
+    const savedBlog = await blog.save()
+
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
+    response.status(201).json(savedBlog)
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      response.status(400).json({ error: error.message })
+    }
+  }
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  const blogId = request.params.id
+
+  try {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'Token invalid' })
+    }
+    const blog = await Blog.findById(blogId)
+    if (!blog) {
+      return response.status(404).json({ error: 'Blog not found' })
+    }
+    if (blog.user.toString() !== decodedToken.id) {
+      return response.status(401).json({ error: 'Unauthorized to delete this blog' })
+    }
+    await Blog.findByIdAndRemove(blogId)
+    response.status(204).end()
+  } catch (error) {
+    return response.status(500).json({ error: 'Server error' })
+  }
+})
+
+blogsRouter.put('/:id', (request, response) => {
+  const body = request.body
+
+  const  blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: body.user
+  }
+
+  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    .then(updatedBlog => {
+      response.json(updatedBlog)
+    })
+})
+
+
+module.exports = blogsRouter
