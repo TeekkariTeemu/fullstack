d2b26ac1886d379112222b3dafdb23087121e25f
TeekkariTeemu <tsoukki@hotmail.com>
2023-06-15 14:06:36 +0300

    5.9 valmis

--- Osa5/blogilista/frontend/src/components/Blog.js
+++ Osa5/blogilista/frontend/src/components/Blog.js
@@ -1,29 +1,31 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
+import blogService from '../services/blogs'
 
 const Blog = ({ blog }) => {
-  const [showDetails, setShowDetails] = useState(false)
+  const [showDetails, setShowDetails] = useState(false);
+  const [updatedLikes, setUpdatedLikes] = useState(blog.likes)
 
-  const toggleDetails = () => {
-    setShowDetails(!showDetails)
+  const handleLike = async () => {
+    const updatedBlog = { ...blog, likes: blog.likes + 1 }
+    await blogService.update(blog.id, updatedBlog)
+    setUpdatedLikes(updatedBlog.likes)
   };
 
   return (
     <div className="blog">
-      <div>
-        {blog.title} {blog.author}
-        <button onClick={toggleDetails}>
-          {showDetails ? 'Hide' : 'Show'}
-        </button>
-      </div>
+      <h3>{blog.title}, by: {blog.author}</h3>
       {showDetails && (
-        <div>
-          <p>URL: {blog.url}</p>
-          <p>Likes: {blog.likes}</p>
-          <p>Author: {blog.author}</p>
-        </div>
+        <>
+          <p>Likes: {updatedLikes} <button onClick={handleLike}>Like</button> </p>
+          <p>{blog.url}</p>
+          {blog.user && <p>Posted by: {blog.user.username}</p>}
+        </>
       )}
+      <button onClick={() => setShowDetails(!showDetails)}>
+        {showDetails ? 'Hide' : 'Show'} Details
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default Blog
