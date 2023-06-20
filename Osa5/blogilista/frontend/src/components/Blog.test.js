c2b508a3e8c9572e654388bd47c5b307178c98c8
TeekkariTeemu <tsoukki@hotmail.com>
2023-06-20 15:29:57 +0300

    5.15 close enough

--- Osa5/blogilista/frontend/src/components/Blog.test.js
+++ Osa5/blogilista/frontend/src/components/Blog.test.js
@@ -3,6 +3,7 @@ import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
 import userEvent from '@testing-library/user-event'
+// import BlogForm from './BlogForm'
 
 test('renders blog title and author by default', () => {
   const blog = {
@@ -36,9 +37,9 @@ test('renders blog URL and number of likes when details button is clicked', asyn
     },
   }
 
-  const mockHandler = jest.fn()
 
-  render(<Blog blog={blog} showDetails={mockHandler} />)
+
+  render(<Blog blog={blog}/>)
 
   const user = userEvent.setup()
   const button = screen.getByText('Show Details')
@@ -50,3 +51,61 @@ test('renders blog URL and number of likes when details button is clicked', asyn
   expect(urlElement).toBeInTheDocument()
   expect(likesElement).toBeInTheDocument()
 })
+
+jest.mock('../services/blogs', () => ({
+  update: jest.fn(),
+}))
+
+test('clicking the button adds a Like', async () => {
+  const blog = {
+    id: 1,
+    title: 'Testi Blogi',
+    author: 'Jaska Jokunen',
+    url: 'https://testi.fi',
+    likes: 10,
+    user: {
+      username: 'jaska',
+    },
+  }
+
+  render(<Blog blog={blog} />)
+
+  const showDetailsButton = screen.getByText('Show Details')
+
+  await userEvent.click(showDetailsButton)
+
+  const likeButton = screen.getByText('Like')
+
+  await userEvent.click(likeButton)
+
+  const likesText = screen.getByText(/Likes: \d+/)
+
+  expect(likesText).toHaveTextContent(`Likes: ${blog.likes + 1}`)
+})
+
+/*
+
+describe('BlogForm', () => {
+  test('calls the event handler with the right details when a new blog is created', () => {
+    const addBlogMock = jest.fn()
+
+    render(<BlogForm addBlog={addBlogMock} />)
+
+    // Simulate user input
+    userEvent.type(screen.getByLabelText('title:'), 'Test Blog Title')
+    userEvent.type(screen.getByLabelText('author:'), 'Test Author')
+    userEvent.type(screen.getByLabelText('url:'), 'https://example.com')
+
+    // Submit the form
+    userEvent.click(screen.getByText('create'))
+
+    // Verify that the event handler was called with the correct details
+    expect(addBlogMock).toHaveBeenCalledWith({
+      title: 'Test Blog Title',
+      author: 'Test Author',
+      url: 'https://example.com',
+    })
+  })
+})
+
+*/
